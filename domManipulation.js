14cb5c084c75ee08700200e6e40332b64017f3c4
rubenhidayat <rubenhidayat02@gmail.com>
2021-09-08 21:41:34 +0700

    domManipulation

---
+++ domManipulation.js
@@ -0,0 +1,17 @@
+// const judul = document.getElementById('judul');
+// judul.innerHTML = '<em>RUBEN HIDAYAT</em>';
+
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML = 'hello world';
+
+
+// const judul = document.querySelector('#judul');
+// judul.style.color = 'lightblue';
+// judul.style.backgroundColor = 'salmon';
+
+
+// const judul = document.getElementsByTagName('h1')[0];
+// const a = document.querySelector('section#a a');
+
+
+const p2 = document.querySelector('.p2');
