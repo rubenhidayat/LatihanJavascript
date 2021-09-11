de638a94f0335c9e829941865bfe927e704141d9
rubenhidayat <rubenhidayat02@gmail.com>
2021-09-11 19:29:00 +0700

    2 addEventListener

--- domEvents.js
+++ domEvents.js
@@ -1,16 +1,30 @@
-//event handler
-const p2 = document.querySelector('.p2');
-function ubahWarna(){
-    p2.style.backgroundColor = 'lightblue';
-}
-p2.onclick = ubahWarna;
+// //event handler
+// const p2 = document.querySelector('.p2');
+// function ubahWarna(){
+//     p2.style.backgroundColor = 'lightblue';
+// }
+// p2.onclick = ubahWarna;
 
-//add event listener
-const p4 = document.querySelector('section#b p');
-p4.addEventListener('click', function(){
-    const ul = document.querySelector('section#b ul');
-    const li = document.createElement('li');
-    const textLi = document.createTextNode('li baru');
-    li.appendChild(textLi);
-    ul.appendChild(li);
+// //add event listener
+// const p4 = document.querySelector('section#b p');
+// p4.addEventListener('click', function(){
+//     const ul = document.querySelector('section#b ul');
+//     const li = document.createElement('li');
+//     const textLi = document.createTextNode('li baru');
+//     li.appendChild(textLi);
+//     ul.appendChild(li);
+// });
+
+
+const p3 = document.querySelector('.p3');
+p3.addEventListener('click', function(){
+    p3.style.backgroundColor = 'lightblue';
 });
+
+p3.addEventListener('click', function(){
+    p3.style.color = 'red';
+});
+
+//LIBRARY https://developer.mozilla.org/en-US/docs/Web/Events
+
+
