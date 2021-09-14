9b7fb1b8c3999dc281083e12c6286e90cce00cc3
rubenhidayat <rubenhidayat02@gmail.com>
2021-09-14 19:25:36 +0700

    dark-mode

--- latihanWarna.js
+++ latihanWarna.js
@@ -1,1 +1,6 @@
+const h1 = document.getElementById('h1');
 const tombol = document.getElementById('ganti');
+tombol.addEventListener('click', function(){
+    // document.body.style.backgroundColor = 'lightblue';
+    document.body.classList.toggle('dark-mode');
+});
