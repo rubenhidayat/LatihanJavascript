ccda6c0b5af03c059af0a010671075cfbc43db87
rubenhidayat <rubenhidayat02@gmail.com>
2021-09-14 19:47:02 +0700

    slider warna

--- latihanWarna.js
+++ latihanWarna.js
@@ -4,3 +4,28 @@ tombol.addEventListener('click', function(){
     // document.body.style.backgroundColor = 'lightblue';
     document.body.classList.toggle('dark-mode');
 });
+
+
+//tombol acak warna
+const tAcakWarna = document.createElement('button');
+const textTombol = document.createTextNode('Acak Warna');
+tAcakWarna.appendChild(textTombol);
+tAcakWarna.setAttribute('type', 'button');
+tombol.after(tAcakWarna);
+
+tAcakWarna.addEventListener('click', function(){
+    const r = Math.round(Math.random() * 255 + 1);
+    const g = Math.round(Math.random() * 255 + 1);
+    const b = Math.round(Math.random() * 255 + 1);
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b + ')';
+});
+
+
+
+//slider warna
+const sMerah = document.querySelector('input[name=sMerah]');
+
+sMerah.addEventListener('change', function(){
+    const r = sMerah.value;
+    document.body.style.backgroundColor = 'rgb('+ r +',0,0)'
+})
