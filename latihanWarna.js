5679d27818957e1c3e300294054b102bce5e071e
rubenhidayat <rubenhidayat02@gmail.com>
2021-09-15 19:45:20 +0700

    rgb

--- latihanWarna.js
+++ latihanWarna.js
@@ -1,5 +1,6 @@
 const h1 = document.getElementById('h1');
 const tombol = document.getElementById('ganti');
+const warna = document.createTextNode('Light Mode');
 tombol.addEventListener('click', function(){
     // document.body.style.backgroundColor = 'lightblue';
     document.body.classList.toggle('dark-mode');
@@ -24,8 +25,26 @@ tAcakWarna.addEventListener('click', function(){
 
 //slider warna
 const sMerah = document.querySelector('input[name=sMerah]');
+const sHijau = document.querySelector('input[name=sHijau]');
+const sBiru = document.querySelector('input[name=sBiru]');
 
-sMerah.addEventListener('change', function(){
+sMerah.addEventListener('input', function(){
     const r = sMerah.value;
-    document.body.style.backgroundColor = 'rgb('+ r +',0,0)'
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ r +', '+ g +', '+ b +')';
 })
+
+sHijau.addEventListener('input', function(){
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ r +', '+ g +', '+ b +')';
+})
+
+sBiru.addEventListener('input', function(){
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ r +', '+ g +', '+ b +')';
+});
