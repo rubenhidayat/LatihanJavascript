61974e1022b6e875d14deb06565f57181899aaef
rubenhidayat <rubenhidayat02@gmail.com>
2021-09-16 19:19:46 +0700

    suit1.0

---
+++ suit.js
@@ -0,0 +1,36 @@
+let ulang = true;
+
+while(ulang == true){
+    let player = prompt('masukkan batu/gunting/kertas');
+
+    let comp = Math.random();
+    if(comp < 0.34){
+        comp = 'batu';
+    }else if(comp >= 0.34 && comp < 0.67){
+        comp = 'gunting';
+    }else{
+        comp = 'kertas';
+    }
+
+    let hasil = '';
+
+    if(player == comp){
+        hasil = 'seri';
+    }else if(player == 'batu'){
+        hasil = (comp == 'gunting') ? 'Menang!' : 'Kalah!';
+    }else if(player == 'gunting'){
+        hasil = (comp == 'kertas') ? 'Menang!' : 'Kalah!';
+    }else if(player == 'kertas'){
+        hasil = (comp == 'batu') ? 'Menang!' : 'Kalah!';
+    }else{
+        hasil = 'ANDA SALAH MEMASUKKAN';
+    }
+
+    alert('Kamu memilih :  '+ player +' dan komputer memilih '+ comp +'\n maka hasilnya adalah : '+hasil+'');
+
+    ulang = confirm('apakah anda ingin bermain lagi??');
+}
+
+alert('terima kasih sudah bermain');
+
+
