e84468f35ab4c5bab6b42eb743c2c0519079c35a
rubenhidayat <rubenhidayat02@gmail.com>
2021-09-27 19:13:30 +0700

    eventBubling

--- domTraversal.js
+++ domTraversal.js
@@ -9,36 +9,36 @@
 // -parentNode                 ->  node
 // -parentElement              ->  element
 // -nextSibling                ->  node
-// -nextElementSibling         ->  element
-// -previousSibling            ->  node
-// -previousElementSibling     ->  element
+// // -nextElementSibling         ->  element
+// // -previousSibling            ->  node
+// // -previousElementSibling     ->  element
 
 
-//dom Traversal
+// //dom Traversal
 
-const close = document.querySelectorAll('.close');
-// for(let i = 0; i<close.length;i++){
-//     close[i].addEventListener('click', function(e){
-//         // close[i].parentElement.style.display = 'none';
+// const close = document.querySelectorAll('.close');
+// // for(let i = 0; i<close.length;i++){
+// //     close[i].addEventListener('click', function(e){
+// //         // close[i].parentElement.style.display = 'none';
+// //         e.target.parentElement.style.display = 'none';
+// //     });
+// // }
+
+// close.forEach(function(el){
+//     el.addEventListener('click', function(e){
 //         e.target.parentElement.style.display = 'none';
+//         e.preventDefault();//menghentikan aktifitas default html
+//         e.stopPropagation();//menghentikan aktifitas fungsi lain pada parent yg sama
 //     });
-// }
-
-close.forEach(function(el){
-    el.addEventListener('click', function(e){
-        e.target.parentElement.style.display = 'none';
-        e.preventDefault();//menghentikan aktifitas default html
-        e.stopPropagation();//menghentikan aktifitas fungsi lain pada parent yg sama
-    });
-});
+// });
 
 
-const cards = document.querySelectorAll('.card');
-cards.forEach(function(card){
-    card.addEventListener('click', function(x){
-        alert('okat');
-    });
-});
+// const cards = document.querySelectorAll('.card');
+// cards.forEach(function(card){
+//     card.addEventListener('click', function(x){
+//         alert('okat');
+//     });
+// });
 
 
 
@@ -53,3 +53,10 @@ cards.forEach(function(card){
 
 
 
+const container = document.querySelector('.container');
+
+container.addEventListener('click', function(e){
+    if(e.target.className == 'close'){
+        e.target.parentElement.style.display = 'none';
+    }
+})
