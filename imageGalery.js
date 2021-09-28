846856d307b6a14fb66ed3fbf4c05ff67bdd6702
rubenhidayat <rubenhidayat02@gmail.com>
2021-09-28 19:56:17 +0700

    active classlist

--- imageGalery.js
+++ imageGalery.js
@@ -1,7 +1,21 @@
 const cont = document.querySelector('.container');
 const jumbo = document.querySelector('.jumbo');
+const thumbs = document.querySelectorAll('.thumb');
 cont.addEventListener('click', function(e){
     if(e.target.className == 'thumb'){
         jumbo.src = e.target.src;
+        jumbo.classList.add('fade');
+        setTimeout(function(){
+            jumbo.classList.remove('fade');
+        }, 500);
+
+        thumbs.forEach(function(thumb){
+            // if(thumb.classList.contains('active')){
+            //     thumb.classList.remove('active')
+            // };
+            thumb.className = 'thumb';
+        })
+
+        e.target.classList.add('active')
     }
 })
