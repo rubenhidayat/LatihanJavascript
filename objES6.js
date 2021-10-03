6a174411265646697227f4725b20907f79d017d3
rubenhidayat <rubenhidayat02@gmail.com>
2021-10-03 19:59:57 +0700

    ts

--- objES6.js
+++ objES6.js
@@ -1,10 +1,35 @@
-let car = {
-    speed: 40,
-    accelerate(){
-        this.speed += 10;
-    }
-};
 
-car.accelerate();
-car.accelerate();
-console.log(car.speed);
+// let car = {
+//     speed: 40,
+//     accelerate(){
+//         this.speed += 10;
+//     }
+// };
+// car.accelerate();
+// car.accelerate();
+// console.log(car.speed);
+
+
+
+
+// function simpleArraySum(ar) {
+//     // Write your code here
+//     let sum = 0;
+//     for(i=0;i<ar.length;i++){
+//         sum += ar[i];
+//     }
+//     alert(sum)
+
+// }
+// x = [1,2,3,4,5];
+// simpleArraySum(x);
+
+
+
+// const obj1 = {
+//     a: 0,
+//     b: 2,
+//     c: 4
+//   };
+//   const obj2 = Object.assign({c: 5, d: 6}, obj1);
+//   console.log(obj2.c, obj2.d);
