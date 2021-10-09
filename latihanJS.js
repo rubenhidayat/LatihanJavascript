0fdbc93421bfaab662f5fbd7f4d575c1c856ac68
rubenhidayat <rubenhidayat02@gmail.com>
2021-10-09 20:18:47 +0700

    destructuring.

--- latihanJS.js
+++ latihanJS.js
@@ -1,24 +1,35 @@
-var duplicateZeros = function(arr){
-    for(i=0;i<arr.length;i++){
-        if(arr[i]==0){
-            arr.splice(i+1, 0, 0);
-            i=i+1           
-        }
-    }
-    if(arr.length>8){
-            arr.splice(arr.length-(arr.length-8));
-    }
-    
-    alert(arr);
+// //DUPLIKASI 0 DIDALAM ARRAY
+// var duplicateZeros = function(arr){
+//     for(i=0;i<arr.length;i++){
+//         if(arr[i]==0){
+//             arr.splice(i+1, 0, 0);
+//             i=i+1  
+//             arr.pop();         
+//         }
+//     }
+//     alert(arr);
+// }
+// var x = [0,0,0,0,0,0,0];
+// duplicateZeros(x);
+// // x.splice(2);
 
-}
-var x = [1,2,3];
-duplicateZeros(x);
-// x.splice(2);
+// // console.log(x);
+// // if(x.length>8){
+// //     x.splice(x.length-(x.length-8));
+// // }
 
-// console.log(x);
-// if(x.length>8){
-//     x.splice(x.length-(x.length-8));
+// // alert(x);
+
+
+//PLUS ONE
+
+// var plusOne = function(arr){
+//     var temp = (arr[arr.length-1]+1);
+//     arr.pop();
+//     arr.push(temp);
+//     alert(arr);
 // }
 
-// alert(x);
+x=[1,2,3,4];
+// plusOne(x);
+alert(x.length);
