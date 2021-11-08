2ad77215b480acfe44b8c4ab3837e8151b3363fb
rubenhidayat <rubenhidayat02@gmail.com>
2021-11-08 18:57:01 +0700

    rest and spread

--- objES6.js
+++ objES6.js
@@ -54,11 +54,19 @@
 // let data = ['ruben hidayat', '123', '23'];
 // signIn(...data);
 
-signIn = (...member)=>{
-    console.log(member);
-}
+// signIn = (...member)=>{
+//     console.log(member);
+// }
 
-username = 'ruben hidayat'
-password = '123'
-umur = '23'
-signIn(username, password, umur);
+// username = 'ruben hidayat'
+// password = '123'
+// umur = '23'
+// signIn(username, password, umur);
+
+
+// function magic(...nums) {
+//   let sum = 0;
+//   nums.filter(n => n % 2 == 0).map(el => sum+= el);
+//   return sum;
+// }
+// console.log(magic(1, 2, 3, 4, 5, 6));
